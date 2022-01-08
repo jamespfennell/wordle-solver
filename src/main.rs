f764860a79d826f36d04b0b3e21c5e602a155283
James Fennell <jamespfennell@gmail.com>
2022-01-08 15:03:44 -0500

    Add a nicer UI for the simulated case

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,136 @@
-use std::fmt::Display;
 use rustc_hash::FxHashMap as HashMap;
 use rustc_hash::FxHashSet as HashSet;
+use std::fmt::Display;
 
-// TODO: recursively calculate the probabilies for subsequently choices --recursive
 // TODO: only make guesses that are valid aka --hard-mode
-// TODO: if there is only 1 solution in each bucket, go into hard mode?
+// TODO: support guessing from the bigger corpus
 
 fn main() {
     println!("Wordle Solver");
+    let valid_answers = parse_words(include_str!("valid_answers.txt"));
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        println!("An input word is required");
-        std::process::exit(1);
-    }
-    let solution_raw: Vec<char> = args[1].chars().collect();
-    if solution_raw.len() != 5 {
-        println!("The input word must have exactly 5 letters");
-        std::process::exit(1);
-    }
-    let solution = Word(solution_raw[0..5].try_into().unwrap());
-    let guesser = KnownWordGuesser{
-        word: &solution,
-    };
+    match args.len() {
+        1 => {
+            println!("An input word is required");
+            std::process::exit(1);
+        }
+        2 => {
+            let solution_raw: Vec<char> = args[1].chars().collect();
+            if solution_raw.len() != 5 {
+                println!("The input word must have exactly 5 letters");
+                std::process::exit(1);
+            }
+            let solution = Word(solution_raw[0..5].try_into().unwrap());
+            // TODO: check that the solution is in the words set
 
-    let valid_answers = include_str!("valid_answers.txt");
-    let words = parse_words(valid_answers);
-    let mut possible_solutions = words.clone();
-    while possible_solutions.len() > 1 {
-        let (best, best_score) = best_guess(&words, &possible_solutions);
-        println!("Guess {} (expected remaining solutions: {})", best, best_score);
+            println!("\nSimulating game play for solution '{}'", solution);
+            let guesser = KnownSolutionGuesser { solution };
+            solve(guesser, valid_answers.clone(), valid_answers);
+        }
+        _ => {
+            println!("Too many inputs passed");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn solve<T: Guesser>(guesser: T, allowed_guesses: Vec<Word>, mut possible_solutions: Vec<Word>) {
+    let mut responses = Vec::new();
+    loop {
+        println!("");
+        let (best, best_score) = best_guess(&allowed_guesses, &possible_solutions);
+        println!(
+            "Guess: {} (expected remaining solutions: {})",
+            best, best_score
+        );
         let response = guesser.guess(best);
+        responses.push(response);
+        println!("Response: {}", response);
+        if response == GuessResponse::solved() {
+            break;
+        }
         let hist = build_histogram(best, &possible_solutions);
-        possible_solutions = hist[&response].iter().map(|w| {(*w).clone()}).collect();
+        possible_solutions = hist[&response].iter().map(|w| (*w).clone()).collect();
         println!("{} remaining solutions", possible_solutions.len());
     }
+
+    println!("");
     println!("Solution: {}", possible_solutions[0]);
+    for response in responses {
+        println!("{}", response);
+    }
 }
 
 trait Guesser {
-    fn guess(&self, word: &Word) -> GuessResponse;
+    fn guess(&self, word: Word) -> GuessResponse;
 }
 
-struct KnownWordGuesser<'a> {
-    word: &'a Word,
+struct KnownSolutionGuesser {
+    solution: Word,
 }
 
-impl<'a> Guesser for KnownWordGuesser<'a> {
-    fn guess(&self, guess: &Word) -> GuessResponse {
+impl Guesser for KnownSolutionGuesser {
+    fn guess(&self, guess: Word) -> GuessResponse {
         let f = |i| {
-            if guess.0[i] == self.word.0[i] {
+            if guess.0[i] == self.solution.0[i] {
                 return GuessResponseChar::Exact;
             }
             for j in 0..5 {
-                if guess.0[i] == self.word.0[j] {
+                if guess.0[i] == self.solution.0[j] {
                     return GuessResponseChar::Included;
                 }
             }
             GuessResponseChar::Excluded
         };
-        GuessResponse([f(0), f(1), f(2), f(3), f(4)]) 
+        GuessResponse([f(0), f(1), f(2), f(3), f(4)])
     }
 }
 
-fn best_guess<'a>(guesses: &'a [Word], possible_solutions: &[Word]) -> (&'a Word, usize) {
-    let mut best = &guesses[0];
-    let mut best_score = possible_solutions.len() * possible_solutions.len();
+struct InteractiveGuesser;
+
+impl Guesser for InteractiveGuesser {
+    fn guess(&self, guess: Word) -> GuessResponse {
+        let f = |i| GuessResponseChar::Excluded;
+        GuessResponse([f(0), f(1), f(2), f(3), f(4)])
+    }
+}
+
+fn best_guess(guesses: &[Word], possible_solutions: &[Word]) -> (Word, f64) {
+    if possible_solutions.len() == 1 {
+        return (possible_solutions[0], 1.0);
+    }
+    let mut best = guesses[0];
+    let mut best_score = usize::MAX;
     for guess in guesses {
-        let hist = build_histogram(guess, possible_solutions);
+        let hist = build_histogram(*guess, possible_solutions);
         let mut expected_remaining = 0;
         for val in hist.values() {
             expected_remaining += val.len() * val.len();
         }
         if expected_remaining < best_score {
-            best = guess;
+            best = *guess;
             best_score = expected_remaining;
         }
     }
-    (best, best_score / possible_solutions.len())
+    (
+        best,
+        (best_score as f64) / (possible_solutions.len() as f64),
+    )
 }
 
-fn build_histogram<'a>(guess: &Word, possible_solutions: &'a [Word]) -> HashMap<GuessResponse, HashSet<&'a Word>> {
+fn build_histogram<'a>(
+    guess: Word,
+    possible_solutions: &[Word],
+) -> HashMap<GuessResponse, HashSet<&Word>> {
     let mut hist = HashMap::default();
     for solution in possible_solutions {
-        let response = KnownWordGuesser {
-            word: solution,
-        }.guess(guess);
-        hist.entry(response).or_insert(HashSet::default()).insert(solution);
+        let response = KnownSolutionGuesser {
+            solution: *solution,
+        }
+        .guess(guess);
+        hist.entry(response)
+            .or_insert(HashSet::default())
+            .insert(solution);
     }
     hist
 }
@@ -109,7 +154,7 @@ fn parse_words(words_txt: &str) -> Vec<Word> {
     words
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 struct Word([char; 5]);
 
 impl Word {
@@ -128,9 +173,24 @@ impl Display for Word {
     }
 }
 
-
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
 struct GuessResponse([GuessResponseChar; 5]);
+
+impl GuessResponse {
+    fn solved() -> GuessResponse {
+        GuessResponse([GuessResponseChar::Exact; 5])
+    }
+}
+
+impl Display for GuessResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}{}{}{}",
+            self.0[0], self.0[1], self.0[2], self.0[3], self.0[4],
+        )
+    }
+}
 
 #[derive(Copy, Clone, Hash, PartialEq, Eq)]
 enum GuessResponseChar {
@@ -140,4 +200,20 @@ enum GuessResponseChar {
     Exact,
     /// The character is in the answer but at a different position than provided
     Included,
+}
+
+impl GuessResponseChar {
+    fn unicode_char(&self) -> char {
+        match self {
+            GuessResponseChar::Excluded => '⬛',
+            GuessResponseChar::Exact => '🟩',
+            GuessResponseChar::Included => '🟨',
+        }
+    }
+}
+
+impl Display for GuessResponseChar {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.unicode_char(),)
+    }
 }
